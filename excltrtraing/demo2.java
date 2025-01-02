74e13deb3c5448ac65d4a7efa4c7af726c964473
varshithreddyt23 <varshithreddyt2005@gmail.com>
2025-01-02 12:21:42 +0530

    Add files via upload
    if else

---
+++ excltrtraing/demo2.java
@@ -0,0 +1,14 @@
+public class demo2 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        if ((num1 > num2)){
+            System.out.println("Num1 is greater"+num1);
+        }else if (num2 > num1){
+            System.out.println("Num2 is greater: "+num2);
+        }else{
+            System.out.println("Num2 is greater"+num1);
+        }
+        
+    }
+}
