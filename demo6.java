d818eb674530cfb3c5db08621f3cebd852c64597
varshithreddyt23 <varshithreddyt2005@gmail.com>
2025-01-03 10:16:15 +0530

    while loop 2

---
+++ demo6.java
@@ -0,0 +1,12 @@
+public class demo6 {
+        public static void main(String[] args) {
+            String name = "Varshith Reddy";
+            String pwd = "VarshithReddy23";
+            while ((name == "Varshith Reddy") && (pwd == "VarshithReddy23")){
+                System.out.println("Hello Varshith Reddy welcome to Malla Reddy University");
+                break;
+            }
+        }
+    }
+    
+
