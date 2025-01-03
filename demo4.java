ad641d917bf97d8582ef580b4d4a3a25528d5887
varshithreddyt23 <varshithreddyt2005@gmail.com>
2025-01-03 09:36:09 +0530

    for loop 1 to 100

---
+++ demo4.java
@@ -0,0 +1,8 @@
+public class demo4 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println("Numbers printing from 1 to 100 :" + i);
+        }
+    }
+}
+
