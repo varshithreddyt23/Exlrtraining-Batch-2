62070bca3fb26b2cc7a9ceee6ab8a11b4095cebc
varshithreddyt23 <varshithreddyt2005@gmail.com>
2025-01-08 10:10:11 +0530

    multiples of 100

---
+++ ps002.java
@@ -0,0 +1,8 @@
+public class ps002 {
+        public static void main(String[] args) {
+            int limit = 10; // Change this value to get more multiples
+            for (int i = 1; i <= limit; i++) {
+                System.out.println(100 * i);
+            }
+        }
+    } 
