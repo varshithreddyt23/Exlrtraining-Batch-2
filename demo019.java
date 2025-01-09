0d0ce4e0f6be2ad8341ae0a45020f12edde1e715
varshithreddyt23 <varshithreddyt2005@gmail.com>
2025-01-09 11:44:51 +0530

    Fibonacei series

---
+++ demo019.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class demo019 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the number of terms: ");
+        int n = sc.nextInt();
+        
+        int firstTerm = 0, secondTerm = 1;
+        System.out.println("Fibonacci Series till " + n + " terms:");
+        
+        for (int i = 1; i <= n; ++i) {
+            System.out.print(firstTerm + " ");
+            
+            // Compute the next term
+            int nextTerm = firstTerm + secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+        }
+        
+        sc.close();
+    }
+}
