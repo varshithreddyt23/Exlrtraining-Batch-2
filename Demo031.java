c35e5dd0ad3b831145a74f2e8012c434a722d95d
varshithreddyt23 <varshithreddyt2005@gmail.com>
2025-02-11 21:10:02 +0530

    Hashset

---
+++ Demo031.java
@@ -0,0 +1,21 @@
+import java.util.HashSet;
+class HashSetDemo1 {
+	void HSetMethod() {
+		HashSet<Object> hashSet = new HashSet<>();
+		hashSet.add(100);
+		hashSet.add("Varshith");
+		hashSet.add(9999.99);
+		System.out.println(hashSet);		
+		for(Object x: hashSet) {
+			System.out.println(x);
+		}
+	}
+}
+
+
+public class Demo031 {
+	public static void main(String[] args) {
+		HashSetDemo1 obj = new HashSetDemo1();
+		obj.HSetMethod();
+		}
+	}
