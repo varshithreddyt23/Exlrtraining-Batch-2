29a2d5e2ac094858154f2c057393c435778d9df0
varshithreddyt23 <varshithreddyt2005@gmail.com>
2025-02-11 21:07:15 +0530

    Add files via upload

---
+++ Demo029.java
@@ -0,0 +1,10 @@
+
+public class Demo029 {
+    public static void main(String[] args) {
+        String[] friends = {"Varshith", "Srujan", "Koushik", "Aryan", "Lokesh", "Abhi", "Meghanath", "Charan", "Rushi", "Vamshi"};
+        System.out.println("List of friends:");
+        for (String friend : friends) {
+            System.out.println(friend);
+        }
+    }
+}
