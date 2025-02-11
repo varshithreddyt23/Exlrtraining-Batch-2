960750a7efe8da83bf8026509c13d4179fd02378
varshithreddyt23 <varshithreddyt2005@gmail.com>
2025-02-11 21:12:15 +0530

    Hashset using string values

---
+++ Demo032.java
@@ -0,0 +1,11 @@
+import java.util.HashSet;
+public class Demo032 {
+    public static void main(String[] args) {
+        HashSet<String> stringSet = new HashSet<>();
+        stringSet.add("Apple");
+        stringSet.add("Banana");
+        stringSet.add("Orange");
+        stringSet.add("Grapes");
+        System.out.println("Elements in the HashSet: " + stringSet);
+    }
+}
